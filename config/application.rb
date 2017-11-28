db9408918981ecaa4f2c5cb1506b9c2316e17156
Patrick Morgan <patrick@patrick-morgan.net>
2017-11-28 16:56:58 -0600

    Use UUIDs for ActiveRecord IDs

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module DruidWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
