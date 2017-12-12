f7693474f3686c1f652e7ce77c84a8d997d0f2a6
Patrick Morgan <patrick@patrick-morgan.net>
2017-12-12 14:22:28 -0600

    annotate property models

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id           :uuid             not null, primary key
+#  name         :string
+#  address1     :string
+#  address2     :string
+#  address3     :string
+#  city         :string
+#  state        :string
+#  zip          :string
+#  country      :string
+#  organization :string
+#  contact_name :string
+#  phone        :string
+#  fax          :string
+#  email        :string
+#  units        :integer
+#  notes        :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
