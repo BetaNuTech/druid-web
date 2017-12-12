b89dad12edd6684bc9757e457f94ae2e3f721341
Patrick Morgan <patrick@patrick-morgan.net>
2017-12-12 13:48:55 -0600

    Update Navigation Bar. Working current path indicators.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
     text, glyph_class = glyph_mappings.fetch(type)
     content_tag(:span, '', {class: glyph_class, "aria-hidden": true})
   end
+
+  def nav_active_class(path)
+    request.path.match(path) ? 'btn-success btn-nav-active' : 'btn-info'
+  end
+
+  def nav_active_dropdown_class(path)
+    request.path.match(path) ? 'btn-success' : 'btn-info'
+  end
 end
