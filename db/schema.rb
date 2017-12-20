dfe3a155a59634b413236f7f6bea2937dd6af445
Patrick Morgan <patrick@patrick-morgan.net>
2017-12-20 15:50:21 -0600

    Add beds to LeadPreference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219204743) do
+ActiveRecord::Schema.define(version: 20171220214311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171219204743) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "beds"
   end
 
   create_table "lead_sources", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
