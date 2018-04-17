aaf8583ee3e8594a03cbb68e581e5e19ee040304
Patrick Morgan <patrick@patrick-morgan.net>
2018-04-17 11:47:20 -0500

    Remove "Add Note" option on ScheduledAction#completion_form

--- app/helpers/scheduled_actions_helper.rb
+++ app/helpers/scheduled_actions_helper.rb
@@ -1,7 +1,7 @@
 module ScheduledActionsHelper
 
   def select_scheduled_action_action(action)
-    options = [ ['Add Note', 'note'], ['Mark Complete', 'complete'], ['Mark Complete and Retry Later', 'complete_retry'], ['Reject', 'reject'] ]
+    options = [ ['Mark Complete', 'complete'], ['Mark Complete and Retry Later', 'complete_retry'], ['Reject', 'reject'] ]
     options_for_select(options, action)
   end
 
