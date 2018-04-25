1756341bfd8288d9e0e2d086bdf14e89dfd856a6
Patrick Morgan <patrick@patrick-morgan.net>
2018-04-25 15:24:38 -0500

    UI improvements to Leads#Show * Pending Task formatting * Hidden comment form, toggled by 'Add' button

--- app/assets/javascripts/leads.js
+++ app/assets/javascripts/leads.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('turbolinks:load', function() {
+    $("#lead_toggle_comments_form_link").on('click', function(e){
+      e.preventDefault();
+      $(e.target).hide()
+      $("#lead_comments_form").toggle('slide');
+    })
+  }
+);
