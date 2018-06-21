f07d730be1a2e5f73844462e0a321ffe139163aa
Patrick Morgan <patrick@patrick-morgan.net>
2018-06-21 10:03:00 -0500

    Add CoStar Lead import adapter

--- app/lib/leads/adapters.rb
+++ app/lib/leads/adapters.rb
@@ -7,7 +7,7 @@ module Leads
     # List valid/enabled adapter classes Here
     ### IMPORTANT: Values in the VALID array correspond directly to
     # the LeadSource record "slug"s
-    SUPPORTED = [ 'Druid', 'Zillow', 'Cloudmailin', 'YardiVoyager' ]
+    SUPPORTED = [ 'Druid', 'Zillow', 'Cloudmailin', 'YardiVoyager', 'Costar' ]
 
 
     # Does the provided source match a valid Lead Adapter Source
---
+++ app/lib/leads/adapters/costar.rb
@@ -0,0 +1,40 @@
+module Leads
+  module Adapters
+    # Reference passthrough data adapter
+    # This class corresponds to a LeadSource record with the slug value 'Costar'
+    class Costar
+      LEAD_SOURCE_SLUG = 'Costar'
+
+      def initialize(params)
+        @property_code = get_property_code(params)
+        @data = filter_params(params)
+      end
+
+      # Return parsed data and meta-information as a Leads::Creator::Result
+      def parse
+        lead = Lead.new(@data)
+        lead.validate
+        status = lead.valid? ? :ok : :invalid
+        result = Leads::Creator::Result.new( status: status, lead: @data, errors: lead.errors, property_code: @property_code)
+        return result
+      end
+
+      private
+
+      def get_property_code(params)
+        return params[:property_id]
+      end
+
+      # Filter for whitelisted params
+      #
+      # (extracted from LeadsController)
+      def filter_params(params)
+        valid_lead_params = Lead::ALLOWED_PARAMS
+        valid_preference_params = [{preference_attributes: LeadPreference::ALLOWED_PARAMS }]
+        filterable_params = params.is_a?(ActionController::Parameters) ? params : ActionController::Parameters.new(params)
+        return filterable_params.permit(*(valid_lead_params + valid_preference_params))
+      end
+
+    end
+  end
+end
