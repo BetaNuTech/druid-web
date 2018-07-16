cfc72bd9fa7c576c2aeef1e23ebc2838afebd32c
Patrick Morgan <patrick@patrick-morgan.net>
2018-07-16 14:43:07 -0500

    Avoid AWS S3 lookup of recording_path validity if there is no recordingfile recorded

--- app/models/concerns/cdrs/aws.rb
+++ app/models/concerns/cdrs/aws.rb
@@ -9,25 +9,29 @@ module Cdrs
       end
 
       def recording_path_key
-        return calldate.strftime("%Y/%m/%d/#{recordingfile}")
+        return recordingfile.present? ? calldate.strftime("%Y/%m/%d/#{recordingfile}") : nil
       end
 
       def recording_path
         # see config/initializers/aws.rb for AWS S3 client configuration
 
         @cached_recording_path ||= (
-          object_info = ::CDRDB_CALL_RECORDING_S3_CLIENT.head_object(
-            bucket: recording_bucket_name,
-            key: recording_path_key
-          ) rescue nil
+          if recording_path_key
+            object_info = ::CDRDB_CALL_RECORDING_S3_CLIENT.head_object(
+              bucket: recording_bucket_name,
+              key: recording_path_key
+            ) rescue nil
 
-          object_info.nil? ?
-              nil :
-              ::CDRDB_CALL_RECORDING_S3_SIGNER.presigned_url(
-                :get_object,
-                bucket: ::CDRDB_CALL_RECORDING_S3_CONFIG[:bucket],
-                key: recording_path_key
-              )
+            object_info.nil? ?
+                nil :
+                ::CDRDB_CALL_RECORDING_S3_SIGNER.presigned_url(
+                  :get_object,
+                  bucket: ::CDRDB_CALL_RECORDING_S3_CONFIG[:bucket],
+                  key: recording_path_key
+                )
+          else
+            nil
+          end
         )
 
         return @cached_recording_path
