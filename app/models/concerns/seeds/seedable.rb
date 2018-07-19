6d472f84659e0a6cdd2cd108d41e53c2519a5b6b
Patrick Morgan <patrick@patrick-morgan.net>
2018-07-19 15:01:18 -0500

    Improve Seedable: output messages to STDOUT. Support ERB interpolation within seed yml files

--- app/models/concerns/seeds/seedable.rb
+++ app/models/concerns/seeds/seedable.rb
@@ -23,9 +23,10 @@ module Seeds
         yaml_path ||= "#{Rails.root}/db/seeds/#{table_name}.yml"
         raise "Data not found: #{yaml_path}" unless File.exist?(yaml_path)
 
-        Rails.logger.info "SEED DATA: Loading #{yaml_path}"
+        msg =  "SEED DATA: Loading #{yaml_path}"
+        Rails.logger.info msg
 
-        seed_data = YAML.load(File.read(yaml_path))
+        seed_data = YAML.load(ERB.new(File.read(yaml_path)).result)
         data_description = seed_data.fetch(table_name.to_sym, {data:[]})
         key_attribute = data_description.fetch(:key, :name)
         data = data_description.fetch(:data)
@@ -42,35 +43,50 @@ module Seeds
               if (old_record = where(key_attribute => record.fetch(key_attribute)).first).present?
                 if old_record.update_attributes(record)
                   if old_record.changed?
-                    Rails.logger.info "SEED LOAD: Updated #{klass_name}[#{old_record.id}] : #{old_record.previous_changes}"
+                    msg =  "SEED LOAD: Updated #{klass_name}[#{old_record.id}] : #{old_record.previous_changes}"
+                    Rails.logger.info msg
                     updated << {id: old_record.id, changes: old_record.previous_changes}
                   else
-                    Rails.logger.info "SEED LOAD: No Change #{klass_name}[#{old_record.id}]"
+                    msg =  "SEED LOAD: No Change #{klass_name}[#{old_record.id}]"
+                    Rails.logger.info msg
                     noops << {id: old_record.id}
                   end
                 else
-                  Rails.logger.info "SEED LOAD: Error Updating #{klass_name}[#{old_record.id}] : #{old_record.errors.to_a}"
+                  msg =  "SEED LOAD: Error Updating #{klass_name}[#{old_record.id}] : #{old_record.errors.to_a}"
+                  puts msg
+                  Rails.logger.info msg
                   errors << {id: old_record.id, errors: old_record.errors.to_a}
                 end
               else
                 new_record = new(record)
                 if new_record.save
-                  Rails.logger.info "SEED LOAD: Created #{klass_name}[#{new_record.id}] : #{record}"
+                  msg =  "SEED LOAD: Created #{klass_name}[#{new_record.id}] : #{record}"
+                  puts msg
+                  Rails.logger.info msg
                   imported << {id: new_record.id, changes: new_record.previous_changes}
                 else
-                  Rails.logger.info "SEED LOAD: Error Creating #{klass_name}[}] : #{new_record.errors.to_a}"
-                  errors << {id: nil, errors: old_record.errors.to_a}
+                  msg =  "SEED LOAD: Error Creating #{klass_name}['#{record[key_attribute]}'] : #{new_record.errors.to_a}"
+                  puts msg
+                  Rails.logger.info msg
+                  errors << {id: nil, errors: new_record.errors.to_a}
                 end
               end
             end
           end
-          Rails.logger.info "SEED LOAD: Noop (#{noops.size}) #{noops.join("\n")}"
-          Rails.logger.info "SEED LOAD: Imported(#{imported.size}) #{imported.join("\n")}"
-          Rails.logger.info "SEED LOAD: Updated(#{updated.size}) #{updated.join("\n")}"
-          Rails.logger.info "SEED LOAD: Errors(#{errors.size}) #{errors.join("\n")}"
+          msgs = [
+            "SEED LOAD: Noop (#{noops.size}) #{noops.join("\n")}",
+            "SEED LOAD: Imported(#{imported.size}) #{imported.join("\n")}",
+            "SEED LOAD: Errors(#{errors.size}) #{errors.join("\n")}",
+            "SEED LOAD: Updated(#{updated.size}) #{updated.join("\n")}"
+          ]
+          msgs.each do |msg|
+            puts msg
+            Rails.logger.info msg
+          end
           return true
         rescue => e
-          Rails.logger.info "SEED LOAD FAILED! Rolled back updates. Error: #{e} #{e.backtrace}"
+          msg =  "SEED LOAD FAILED! Rolled back updates. Error: #{e} #{e.backtrace}"
+          Rails.logger.info msg
           return false
         end
 
