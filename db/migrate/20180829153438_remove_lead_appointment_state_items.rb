4fa92c2038f067ee5b449cc515a220efce36dba7
Patrick Morgan <patrick@patrick-morgan.net>
2018-08-29 11:11:15 -0500

    Fix migration

--- db/migrate/20180829153438_remove_lead_appointment_state_items.rb
+++ db/migrate/20180829153438_remove_lead_appointment_state_items.rb
@@ -3,8 +3,8 @@ class RemoveLeadAppointmentStateItems < ActiveRecord::Migration[5.2]
     puts "*** Removing Appointment Engagement Policy and associated records"
     EngagementPolicy.where(lead_state: 'appointment').last.destroy
 
-    puts "*** Loading New EngagementPolicy from #{filename}"
     filename = File.join(Rails.root,"db","seeds", "engagement_policy.yml")
+    puts "*** Loading New EngagementPolicy from #{filename}"
     loader = EngagementPolicyLoader.new(filename)
     loader.call
   end
