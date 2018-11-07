6950fa503d8aa58bc2277c59d6f773a8cc613426
Patrick Morgan <patrick@patrick-morgan.net>
2018-11-07 08:51:41 -0600

    Add Zumper ILS Cloudmailin parser

---
+++ app/lib/leads/adapters/cloud_mailin/zumper_parser.rb
@@ -0,0 +1,68 @@
+module Leads
+  module Adapters
+    module CloudMailin
+
+      class ZumperParser
+        def self.match?(data)
+          return data.fetch("headers",{}).fetch("Subject","").
+            match(/Zumper/).
+            present?
+        end
+
+        def self.parse(data)
+          body = data.fetch('html','')
+          message_id = data.fetch('headers',{}).fetch("Message-ID","").strip
+          _, first_name, last_name, email = (data.fetch("headers", {}).fetch("From","").match(/"(\w+) (\w+) <([^>]+)>/).to_a rescue [nil, '', '', ''])
+          title = nil
+          referral = "Zumper.com"
+          phone1 = nil
+          phone2 = nil
+          fax = nil
+          if (beds_baths = (body.match(/The following lead[^:]+[^>]+>[^:]+: ([^<]+)/)[1] rescue '')).empty?
+            beds = nil
+            baths = nil
+          else
+            _, beds, baths = ( beds_baths.match(/(\d).+(\d)/).to_a rescue [nil, nil, nil])
+            beds = beds.to_i
+            baths = baths.to_i
+          end
+          notes = self.sanitize(( body.match(/padding: 13px">(.*)\s+<\/p>\s+<p st/m)[1] rescue '(None)' ).strip.gsub("\n"," "))
+          smoker = nil
+          pets = nil
+          move_in = nil
+          agent_notes = message_id.empty? ? nil : "/// Message-ID: #{message_id}"
+          raw_data = data.to_json
+
+          parsed = {
+            title: title,
+            first_name: first_name,
+            last_name: last_name,
+            referral: "Zumper.com",
+            phone1: phone1,
+            phone1_type: nil,
+            phone2: phone2,
+            phone2_type: nil,
+            email: email,
+            fax: fax,
+            notes: agent_notes,
+            preference_attributes: {
+              baths: baths,
+              beds: beds,
+              notes: notes,
+              smoker: smoker,
+              raw_data: raw_data,
+              pets: pets,
+              move_in: move_in
+            }
+          }
+
+          return parsed
+        end
+
+        def self.sanitize(value)
+          return ActionController::Base.helpers.sanitize(value)
+        end
+      end
+    end
+  end
+end
---
+++ spec/adapters/leads_adapters_cloudmailin_zumper_parser_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Leads::Adapters::CloudMailin::ZumperParser do
+
+  let(:email_data) {
+    JSON.parse File.read(File.join(Rails.root, 'spec', 'support', 'test_data', 'cloudmailin_adapter_zumper_parser_data.json'))
+  }
+
+  describe "parsing email data" do
+    it "should parse the data" do
+      adapter = Leads::Adapters::CloudMailin::ZumperParser
+
+      result = adapter.parse(email_data)
+      ap result
+      expect(result[:title]).to eq(nil)
+      expect(result[:first_name]).to eq('Josh')
+      expect(result[:last_name]).to eq('Pannell')
+      expect(result[:email]).to eq('joshuajpannell@gmail.com')
+      expect(result[:phone1]).to be_nil
+      expect(result[:notes]).to match(/01000166/)
+      expect(result[:preference_attributes][:notes]).to match(/We are looking/)
+      expect(result[:preference_attributes][:beds]).to eq(2)
+      expect(result[:preference_attributes][:baths]).to eq(2)
+    end
+  end
+end
