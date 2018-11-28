599ec3bfcc5469de965b09755d853b12df747c7c
Patrick Morgan <patrick@patrick-morgan.net>
2018-11-28 14:41:24 -0600

    Better Lead duplicate detection and recursion loop protection

--- app/models/concerns/leads/duplicates.rb
+++ app/models/concerns/leads/duplicates.rb
@@ -11,19 +11,37 @@ module Leads
       DUPLICATE_ATTRIBUTES = %w{phone1 phone2 email first_name last_name}
 
       def possible_duplicates
+        invalid_values = ['Null', '00000000', '0000000000', '(None)', 'None']
+        invalid_values_sql = invalid_values.map{|v| "'#{v}'"}.join(", ")
         sql_template =<<~SQL
 
           SELECT leads.id
           FROM leads
           WHERE
-          ( id != :id )
-          AND
-          (
-            ( phone1 = :phone1 AND phone1 != 'Null' AND phone1 != '00000000' AND phone1 != '0000000000' AND phone1 != '' AND phone1 IS NOT NULL ) OR
-            ( phone2 = :phone2 AND phone2 != 'Null' AND phone2 != '00000000' AND phone2 != '0000000000' AND phone2 != '' AND phone2 IS NOT NULL) OR
-            ( first_name = :first_name AND last_name = :last_name AND first_name != 'Null' AND first_name IS NOT NULL) OR
-            ( email = :email AND email != 'Null' AND email IS NOT NULL )
-          )
+            ( id != :id )
+            AND (
+              ( phone1 IS NOT NULL
+                AND phone1 != ''
+                AND phone1 = :phone1
+                AND phone1 NOT IN (#{invalid_values_sql})
+              )
+              OR ( phone2 IS NOT NULL
+                   AND phone2 != ''
+                   AND phone2 = :phone2
+                   AND phone2 NOT IN (#{invalid_values_sql})
+                 )
+              OR ( first_name IS NOT NULL
+                   AND first_name != ''
+                   AND first_name = :first_name
+                   AND last_name = :last_name
+                   AND first_name NOT IN (#{invalid_values_sql})
+                 )
+              OR ( email IS NOT NULL
+                   AND email != ''
+                   AND email = :email
+                   AND email NOT IN (#{invalid_values_sql})
+                 )
+           )
           SQL
 
         query_array = [
@@ -40,7 +58,7 @@ module Leads
         return Lead.where(id: lead_ids)
       end
 
-      def mark_duplicates
+      def mark_duplicates(recurse=true)
         detected = possible_duplicates
         transaction do
           old_duplicates = duplicates.to_a
@@ -54,9 +72,11 @@ module Leads
           # Delete stale DuplicateLead records
           duplicate_records.where("lead_id NOT IN (?)", detected_ids).destroy_all
 
-          # Update XOR'ed list of old and newly detected duplicates
-          refresh_leads = ( (old_duplicates + detected) - ( old_duplicates & detected) ).uniq
-          refresh_leads.each{|od| od.mark_duplicates}
+          if recurse
+            # Update XOR'ed list of old and newly detected duplicates
+            refresh_leads = ( (old_duplicates + detected) - ( old_duplicates & detected) ).uniq
+            refresh_leads.each{|od| od.mark_duplicates(false)}
+          end
         end
 
         return detected
