e36e06439eb9f86cdcd410feafc3aa8d2572e05b
Patrick Morgan <patrick@patrick-morgan.net>
2018-12-28 11:33:14 -0600

    Increment version to 0.9.37

--- app/assets/javascripts/scheduled_actions.js
+++ app/assets/javascripts/scheduled_actions.js
@@ -1,20 +1,5 @@
 $(document).on('turbolinks:load', function() {
 
-  var scheduled_action_completion_action_selector = $('#scheduled_action_completion_action');
-  var retry_delay_selector_container = $('.retry_delay_selector');
-
-  retry_delay_selector_container.hide();
-
-  scheduled_action_completion_action_selector.on('change', function(e){
-    // on Mark Task as Completed selection change
-    var target = e.target;
-    if (e.target.value == 'retry') {
-      retry_delay_selector_container.show()
-    } else {
-      retry_delay_selector_container.hide();
-    }
-  });
-
   function schedule_conflict_check(e){
     var scheduled_action_form = $("#scheduled_action_form");
     if (scheduled_action_form[0] == undefined) return(true);
@@ -27,14 +12,11 @@ $(document).on('turbolinks:load', function() {
         dataType: 'json',
       })
       .done(function(data){
-        console.log('Conflicts?: ',data);
         if (data == true) {
-          console.log("Conflict Found");
           $("select[id^=scheduled_action_schedule_attributes_date]").addClass("scheduling_conflict");
           $("select[id^=scheduled_action_schedule_attributes_time]").addClass("scheduling_conflict");
           $("#schedule_conflict_message").addClass("scheduling_conflict");
         } else {
-          console.log("Conflict Not Found")
           $("select[id^=scheduled_action_schedule_attributes_date]").removeClass("scheduling_conflict");
           $("select[id^=scheduled_action_schedule_attributes_time]").removeClass("scheduling_conflict");
           $("#schedule_conflict_message").removeClass("scheduling_conflict");
@@ -42,8 +24,21 @@ $(document).on('turbolinks:load', function() {
       })
   }
 
+  var retry_delay_selector_container = $('.retry_delay_selector');
+  retry_delay_selector_container.hide();
+
   var schedule_selectors = $("select[name^='scheduled_action[schedule_attributes]'");
   schedule_selectors.on('change', schedule_conflict_check);
-  schedule_conflict_check();
 
+  var scheduled_action_completion_action_selector = $('#scheduled_action_completion_action');
+  scheduled_action_completion_action_selector.on('change', function(e){
+    // on Mark Task as Completed selection change
+    var target = e.target;
+    if (e.target.value == 'retry') {
+      retry_delay_selector_container.show()
+    } else {
+      retry_delay_selector_container.hide();
+    }
+  });
+  schedule_conflict_check();
 });
--- config/version.rb
+++ config/version.rb
@@ -1,3 +1,3 @@
 module DruidWeb
-  VERSION = "0.9.36"
+  VERSION = "0.9.37"
 end
