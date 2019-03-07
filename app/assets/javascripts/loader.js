cf27d265b7b1b17edc5bd452798bfe426c122278
Patrick Morgan <patrick@patrick-morgan.net>
2019-03-07 14:31:58 -0600

    Remove debug console messages from JS Loader

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,17 +1,13 @@
 window.Loader = new Object({
   start: function() {
-    console.log('starting loader')
     var loader_div = document.getElementById("grid-loader");
     var loader_html = "<div class=\"grid-loader-anim\"><div></div><div></div><div></div></div>";
 
     if (loader_div == undefined) {
-      console.log("inserting loader element");
       loader_div = document.createElement('div');
       loader_div.setAttribute("id", "grid-loader");
       loader_div.innerHTML = loader_html;
       document.body.appendChild(loader_div);
-    } else {
-      console.log("loader element found");
     }
 
     loader_div.style.top = ( window.innerHeight / 3 ) + 'px';
@@ -20,7 +16,6 @@ window.Loader = new Object({
     return true;
   },
   stop: function() {
-    console.log('stopping loader')
     var loader_div = document.getElementById("grid-loader");
     if (loader_div != undefined) {
       $(loader_div).fadeOut('fast')
