73e5eda8be1676a524760e760ff068b144b57c34
Patrick Morgan <patrick@patrick-morgan.net>
2019-03-21 14:58:17 -0500

    tweak migration

--- db/migrate/20190306205338_create_property_users.rb
+++ db/migrate/20190306205338_create_property_users.rb
@@ -1,12 +1,18 @@
 class CreatePropertyUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :property_users, id: :uuid do |t|
-      t.uuid :property_id
-      t.uuid :user_id
-      t.integer :role
-      t.timestamps
-    end
+  def self.up
+    unless table_exists?(:property_users)
+      create_table :property_users, id: :uuid do |t|
+        t.uuid :property_id
+        t.uuid :user_id
+        t.integer :role
+        t.timestamps
+      end
 
-    add_index :property_users, [:property_id, :user_id], unique: true
+      add_index :property_users, [:property_id, :user_id], unique: true
+    end
+  end
+
+  def self.down
+    drop_table :property_users
   end
 end
