6c4e8f8b9a9cfd2a90e2d36d3d4f9aec5726138d
Patrick Morgan <patrick@patrick-morgan.net>
2019-04-03 14:56:09 -0500

    Increase call log frequency and remove duplicates from call logs

--- app/models/concerns/leads/call_log.rb
+++ app/models/concerns/leads/call_log.rb
@@ -4,7 +4,7 @@ module Leads
 
     included do
 
-      CALL_LOG_FREQUENCY = 10 # minutes
+      CALL_LOG_FREQUENCY = 5 # minutes
 
       scope :recent_recordings, -> (start_time=1.week.ago) {
         leads = {}
@@ -40,11 +40,14 @@ module Leads
         if should_update_call_log?
           transaction do
             self.call_log = Cdr.calls_for([phone1, phone2]).
+              to_a.
+              uniq{|l| l.calldate}.
               map{|cdr|
               { id: cdr.id,
                 date: cdr.calldate,
                 src: cdr.src,
                 dst: cdr.dst,
+                cnam: cdr.cnam,
                 disposition: cdr.disposition,
                 recordingfile: cdr.recordingfile,
                 recording_path: cdr.recording_path,
