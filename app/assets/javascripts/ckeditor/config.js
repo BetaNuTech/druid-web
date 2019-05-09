0a6c4b81e495aa6bb842a8fd7f469c08abf79260
Patrick Morgan <patrick@patrick-morgan.net>
2019-05-09 11:43:54 -0500

    Ckeditor tweaks

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,23 @@
+CKEDITOR.editorConfig = function( config )
+{
+  config.language = 'en';
+  config.allowedContent = true;
+  config.extraAllowedContent = 'div(*){*}[*]';
+  config.disableNativeSpellChecker = false;
+
+  config.toolbar_mailedit = [
+    { name: 'paragraph', groups: [ 'list', 'indent', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+    { name: 'styles', items: [ 'Font', 'FontSize' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+  ];
+
+  config.toolbar = "mailedit";
+
+};
+
+$(document).on('turbolinks:load', function() {
+ if (document.getElementById('html_editor') != undefined) {
+    CKEDITOR.replace('html_editor')
+  }
+});
