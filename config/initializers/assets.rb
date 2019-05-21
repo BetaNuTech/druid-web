c5cb850017b4b691812e5114cd54d2b11558f355
Patrick Morgan <patrick@patrick-morgan.net>
2019-05-21 12:00:16 -0500

    Add .svg and .swf to precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,8 @@ Rails.application.config.assets.version = '1.1'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( ckeditor/config.js)
+Rails.application.config.assets.precompile += %w('.svg')
+Rails.application.config.assets.precompile += %w('.swf')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
