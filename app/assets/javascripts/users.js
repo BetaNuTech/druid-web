d6352ec9354f35e72d1cae42094b7483366dc277
Patrick Morgan <patrick@patrick-morgan.net>
2020-04-13 11:20:42 -0700

    Remove code that disables the User Signature editor
    Fixes Issue #621

--- app/assets/javascripts/users.js
+++
@@ -1,8 +0,0 @@
-$(document).on('turbolinks:load', function() {
-
-  // User Profile: Message Signature input behaviors
-  $('.user_signature').prop('disabled', !$('.user_signature_enabled').prop('checked'));
-  $('.user_signature_enabled').on('change', function(){
-    $('.user_signature').prop('disabled', !$('.user_signature_enabled').prop('checked'));
-  })
-});
