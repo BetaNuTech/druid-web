7a13a3132ed5354ec619ac97cbe68c3274a39e64
Patrick Morgan <patrick@patrick-morgan.net>
2020-06-08 16:06:26 -0700

    Fix ActionCable/Rspec deprecation error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require 'rspec/rails'
-require 'action_cable/testing/rspec'
 require 'devise'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
