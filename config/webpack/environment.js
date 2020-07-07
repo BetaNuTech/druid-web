3c62bcf717f4bb92b27e5f708c81d9067f720885
Patrick Morgan <patrick@patrick-morgan.net>
2020-07-07 15:56:00 -0700

    Webpack configuration update

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,8 +3,9 @@ const sassLoader = environment.loaders.get('sass')
 const cssLoader = sassLoader.use.find(loader => loader.loader === 'css-loader')
 
 cssLoader.options = Object.assign(cssLoader.options, {
-  modules: true,
-  localIdentName: '[path][name]__[local]--[hash:base64:5]'
+  modules: {
+    localIdentName: '[path][name]__[local]--[hash:base64:5]'
+  }
 })
 
 module.exports = environment
