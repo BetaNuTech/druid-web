e2f6a8a5d47b555ceaed54666cd0634a9e6b56fb
Patrick Morgan <patrick@patrick-morgan.net>
2020-09-14 16:02:49 -0700

    Disable CDR database in test

--- app/models/cdrdb_model.rb
+++ app/models/cdrdb_model.rb
@@ -1,4 +1,4 @@
 class CdrdbModel < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection ENV.fetch('CDRDB_URL','')
+  establish_connection ENV.fetch('CDRDB_URL','') unless Rails.env.test?
 end
