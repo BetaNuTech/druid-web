f691857798c036a4781bc63988089888a278794f
Patrick Morgan <patrick@patrick-morgan.net>
2020-09-16 15:55:24 -0700

    Include UserProfile setting initialization in migration

--- db/migrate/20200915193004_add_appsettings_to_user_profile.rb
+++ db/migrate/20200915193004_add_appsettings_to_user_profile.rb
@@ -2,16 +2,14 @@ class AddAppsettingsToUserProfile < ActiveRecord::Migration[6.0]
   def change
     add_column :user_profiles, :appsettings, :jsonb, default: {}
 
-    UserProfile.where(signature_enabled: true).each do |profile|
-      profile.switch_setting!(:message_signature, true)
-    end
-    remove_column :user_profiles, :signature_enabled
-
-    UserProfile.where(monitor_all_messages: true).each do |profile|
+    UserProfile.all.each do |profile|
+      profile.switch_setting!(:message_signature, true) if profile.signature_enabled?
+      profile.switch_setting!(:view_all_messages, true) if profile.monitor_all_messages?
       profile.switch_setting!(:message_web_notifications, true)
-      profile.switch_setting!(:view_all_messages, true)
+      profile.switch_setting!(:lead_web_notifications, true)
     end
-    remove_column :user_profiles, :monitor_all_messages
 
+    remove_column :user_profiles, :signature_enabled
+    remove_column :user_profiles, :monitor_all_messages
   end
 end
