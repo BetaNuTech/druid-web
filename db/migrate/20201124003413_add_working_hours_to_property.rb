36f4b02d4343449ae880ea63a1a8e5c7de4b45e2
Patrick Morgan <patrick@patrick-morgan.net>
2020-11-30 12:22:18 -0800

    Add Property office hours seed data

--- db/migrate/20201124003413_add_working_hours_to_property.rb
+++ db/migrate/20201124003413_add_working_hours_to_property.rb
@@ -1,5 +1,79 @@
 class AddWorkingHoursToProperty < ActiveRecord::Migration[6.0]
   def change
     add_column :properties, :working_hours, :jsonb
+    Property.update_all(
+      working_hours: 
+      {
+        "monday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "tuesday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "wednesday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "thursday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "friday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "saturday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        },
+        "sunday" => {
+          "morning" => {
+            "open" => "7:00 AM",
+            "close" => "12:00 PM"
+          },
+          "afternoon" => {
+            "open" => "12:00 PM",
+            "close" => "6:00 PM"
+          }
+        }
+      })
   end
 end
