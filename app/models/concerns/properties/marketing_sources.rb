d9e1890cce158606880601049784af6956b8e028
Patrick Morgan <patrick@patrick-morgan.net>
2020-12-16 11:33:01 -0800

    HOTFIX: return Property listing code when returning Property info from an incoming number

--- app/models/concerns/properties/marketing_sources.rb
+++ app/models/concerns/properties/marketing_sources.rb
@@ -15,17 +15,21 @@ module Properties
 
         return { dialed: clean_number, error: 'Not Found'} unless property
 
+        lead_source = LeadSource.where(slug: 'CallCenter').first
+        property_listing = PropertyListing.where(property_id: property.id, source_id: lead_source&.id).first
+
         {
           version: '1.0.0',
           date: Time.now,
           dialed: clean_number,
+          property_id: property_listing&.code,
           name: property.name,
           main_number: property.phone,
           maintenance_number: property.maintenance_phone,
           hours: property.office_hours_today,
           open: property.office_open?,
           rings: 3,
-          referrer: marketing_source&.name || 'Direct'
+          referrer: marketing_source&.name || 'Direct',
         }
       end
 
