8b9374886df51cf2f7ecf59f6cb61ef86294e079
Patrick Morgan <patrick@patrick-morgan.net>
2021-06-08 15:46:11 -0700

    Send notification when employee email address is changed.
    Now sends a notification email to an agent's property manager, or a
    property manager's TRM if their email address was changed. This is a
    security mechanism to mitigate account takeovers.
    
    Fixes #785

--- app/models/concerns/users/properties.rb
+++ app/models/concerns/users/properties.rb
@@ -61,6 +61,42 @@ module Users
         User.includes(:assignments).where(property_users: {property_id: properties.pluck(:id)}).pluck(:id)
       end
 
+      # Override Devise hook for sending a reconfirmation after email address is changed
+      def send_reconfirmation_instructions
+        super
+        send_email_reconfirmation_notification_to_manager
+      end
+
+      # Send notification of email change to managers or TRMs
+      def send_email_reconfirmation_notification_to_manager
+        return true unless self.unconfirmed_email.present?
+
+        recipient = nil
+        if property_agent?
+          recipient = property.managers.map(&:email).join(',') rescue nil
+        elsif property_manager?
+          recipient = property.team.teamleads.map(&:email).join(',') rescue nil
+        end
+        return true unless recipient
+
+        message = Message.new(
+          message_type: MessageType.email,
+          threadid: nil,
+          subject: 'Security Alert: A Bluesky user changed their email address',
+          body: "This is a system/security notification that #{self.name} changed their email address from #{self.email} to #{self.unconfirmed_email}. If this is unexpected, please confirm with the employee by phone or in person.",
+          classification: 'system',
+          user: self,
+          messageable: self,
+          incoming: false
+        )
+        message.senderid = message.outgoing_senderid
+        message.recipientid = recipient
+        message.save!
+        message.deliver!
+
+        return true
+      end
+
     end
   end
 end
