2410ef1b9b839c43f23b03b03b2d67abd9ec0290
Patrick Morgan <patrick@patrick-morgan.net>
2021-07-27 16:45:51 -0700

    Unit test fix

--- spec/models/lead_policy_spec.rb
+++ spec/models/lead_policy_spec.rb
@@ -5,35 +5,38 @@ RSpec.describe LeadPolicy do
 
   describe "policy" do
     describe "allowed params for a new Lead" do
-      let(:lead_params) { 
+      let(:admin_lead_params) { 
         Lead::ALLOWED_PARAMS + [{preference_attributes: LeadPreference::ALLOWED_PARAMS }]
+      }
+      let(:agent_lead_params) { 
+        Lead::ALLOWED_PARAMS + [{preference_attributes: LeadPreference::ALLOWED_PARAMS - [:optout_email, :optin_sms] }]
       }
 
       describe "for administrators" do
         it "should allow all params" do
           policy = LeadPolicy.new(administrator, Lead)
-          expect(policy.allowed_params).to eq(lead_params)
+          expect(policy.allowed_params).to eq(admin_lead_params)
         end
       end
 
       describe "for corporate users" do
         it "should allow all params" do
           policy = LeadPolicy.new(corporate, Lead)
-          expect(policy.allowed_params).to eq(lead_params)
+          expect(policy.allowed_params).to eq(admin_lead_params)
         end
       end
 
       describe "for managers" do
         it "should allow all params" do
           policy = LeadPolicy.new(manager, Lead)
-          expect(policy.allowed_params).to eq(lead_params)
+          expect(policy.allowed_params).to eq(agent_lead_params)
         end
       end
 
       describe "for agents" do
         it "should allow all params" do
           policy = LeadPolicy.new(manager, Lead)
-          expect(policy.allowed_params).to eq(lead_params)
+          expect(policy.allowed_params).to eq(agent_lead_params)
         end
       end
     end
