b065f54f3ef1d8409d75252d652b76c8a936ab82
Patrick Morgan <patrick@patrick-morgan.net>
2022-03-24 14:23:01 -0700

    Added support for new Lineups.io lead email variant

--- app/lib/leads/adapters/cloud_mailin/lineupsio_parser.rb
+++ app/lib/leads/adapters/cloud_mailin/lineupsio_parser.rb
@@ -12,14 +12,28 @@ module Leads
         end
 
         def self.parse(data)
-          return parse_html(data)
+          case variant(data)
+          when 1
+            parse_variant_1(data)
+          when 2
+            parse_variant_2(data)
+          end
+        end
+
+        def self.variant(data)
+          body = data['html']
+          if body.match(/BLUESKY_PROSPECT/)
+            return 2
+          else
+            return 1
+          end
         end
 
         def self.sanitize(value)
           return ActionController::Base.helpers.sanitize(value)
         end
 
-        def self.parse_html(data)
+        def self.parse_variant_1(data)
 
           begin
             body = data['html']
@@ -29,6 +43,72 @@ module Leads
             message_id = data.fetch('headers',{}).fetch("Message-ID","").strip
 
             embedded_lead_data = doc.xpath('//comment()[contains(.,\'PROSPECT\')]').inner_text&.sub(/PROSPECT = /i,'')&.gsub("\n",'') || ''
+            lead_data = JSON.parse(embedded_lead_data)
+
+            first_name = lead_data['first_name']
+            last_name = lead_data['last_name']
+            phone1 = lead_data['cell_phone']
+            email = lead_data['email']
+            notes = lead_data['comments']
+            move_in = lead_data['desired_move_in']
+            beds = lead_data['desired_bedrooms']
+          rescue => e
+            ErrorNotification.send(e, {message: 'Error parsing lead from Lineups.io'})
+            first_name = 'Error'
+            last_name = 'Error'
+            phone1 = nil
+            email = 'Error'
+            notes = nil
+            move_in = nil
+            beds = nil
+          end
+
+          remoteid = nil
+          title = nil
+          baths = nil
+          phone2 = nil
+          fax = nil
+          pets = nil
+          smoker = nil
+          raw_data = data.to_json
+
+          parsed = {
+            title: title,
+            first_name: first_name,
+            last_name: last_name,
+            referral: referral,
+            phone1: phone1,
+            phone1_type: 'Cell',
+            phone2: phone2,
+            phone2_type: 'Cell',
+            email: email,
+            fax: fax,
+            notes: nil,
+            remoteid: remoteid,
+            preference_attributes: {
+              baths: baths,
+              beds: beds,
+              notes: notes,
+              smoker: smoker,
+              raw_data: raw_data,
+              pets: pets,
+              move_in: move_in
+            }
+          }
+
+          return parsed
+        end
+
+        def self.parse_variant_2(data)
+
+          begin
+            body = data['html']
+            doc = Nokogiri::XML(body)
+
+            referral = REFERRAL
+            message_id = data.fetch('headers',{}).fetch("Message-ID","").strip
+
+            embedded_lead_data = doc.xpath('//comment()[contains(.,\'BLUESKY_PROSPECT\')]').inner_text&.sub(/BLUESKY_PROSPECT = /i,'')&.gsub("\n",'') || ''
             lead_data = JSON.parse(embedded_lead_data)
 
             first_name = lead_data['first_name']
