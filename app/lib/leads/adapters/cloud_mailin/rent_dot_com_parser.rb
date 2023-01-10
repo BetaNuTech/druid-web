28fbfd8ad710677080d8c3cbda4ed19e6ec3af30
Patrick Morgan <patrick@patrick-morgan.net>
2023-01-10 11:57:46 -0800

    Update Rent.com parser for new email format

--- app/lib/leads/adapters/cloud_mailin/rent_dot_com_parser.rb
+++ app/lib/leads/adapters/cloud_mailin/rent_dot_com_parser.rb
@@ -13,6 +13,85 @@ module Leads
         def self.parse(data)
           body = data.fetch(:html,'')
 
+          if ( body.match?(/checked you guys out/) rescue false )
+            return self.parse_html_v2(data)
+          else
+            return self.parse_html_v1(data)
+          end
+
+        end
+
+        def self.parse_html_v2(data)
+          body = data.fetch(:html,'')
+          html = Nokogiri::HTML(body)
+
+          container = html.css('table')
+
+          referral = "Rent.com"
+          message_id = data.fetch(:headers,{}).fetch("Message-ID","").strip
+
+          raw_name = container.css('tr:nth-child(6) strong').text
+          name_arr = raw_name.split(' ')
+          if name_arr.length > 2
+            first_name = ( name_arr[0..1].amp(&:chomp).join(' ') rescue '' )
+          else
+            first_name = ( name_arr.first.chomp rescue '' )
+          end
+          last_name = ( name_arr.last.chomp rescue '' )
+
+          email_raw = container.css('tr:nth-child(7)').text
+          email = (email_raw.match(/Email:.([^\n\t ]+)/)[1] rescue '(None)').strip
+
+          phone_raw = container.css('tr:nth-child(8)').text
+          phone1 = (phone_raw.match(/Phone:.([^\n\t]+)/)[1] rescue '(None)').strip
+
+          move_in_raw = container.css('tr:nth-child(10)').text
+          move_in = (move_in_raw.match(/Move Date:.([^\n\t]+)/)[1] rescue nil).strip
+
+          notes_raw = container.css('tr:nth-child(11)').text
+          notes = (notes_raw.match(/Comments:.([^\n\t]+)/)[1] rescue '(None)').strip.gsub("\n"," ")
+
+          agent_notes = message_id.empty? ? nil : "/// Message-ID: #{message_id}"
+
+          title = nil
+          phone2 = nil
+          fax = nil
+          baths = nil
+          beds = nil
+          smoker = nil
+          pets = nil
+
+          raw_data = data.to_json
+
+          parsed = {
+            title: title,
+            first_name: first_name,
+            last_name: last_name,
+            referral: "Rent.com",
+            phone1: phone1,
+            phone1_type: 'Cell',
+            phone2: phone2,
+            phone2_type: 'Cell',
+            email: email,
+            fax: fax,
+            notes: agent_notes,
+            preference_attributes: {
+              baths: baths,
+              beds: beds,
+              notes: notes,
+              smoker: smoker,
+              raw_data: raw_data,
+              pets: pets,
+              move_in: move_in
+            }
+          }
+
+          return parsed
+        end
+
+        def self.parse_html_v1(data)
+          body = data.fetch(:html,'')
+
           name = ( body.match(/Information for.+?<strong>(.+?)<\/strong>/)[1] rescue '(None)' ).gsub('*','')
           name_arr = name.split(' ')
 
@@ -32,6 +111,7 @@ module Leads
           pets = nil
           move_in = (Date.parse(body.match(/Move Date.+?<\/span>(.*?)<\/td>/m)[1]) rescue nil)
           agent_notes = message_id.empty? ? nil : "/// Message-ID: #{message_id}"
+
           raw_data = data.to_json
 
           parsed = {
