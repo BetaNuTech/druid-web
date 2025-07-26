f1b7a5e6df5314913e306ad445deb724894c15e3
Stu Carney <webstu@gmail.com>
2025-07-26 10:12:41 -0500

    Fix PostgreSQL boolean comparison in system_user index
    Use 'IS TRUE' instead of '= true' for PostgreSQL boolean comparison
    in partial index WHERE clause

--- db/migrate/20250725185322_add_system_user_flag_to_users.rb
+++ db/migrate/20250725185322_add_system_user_flag_to_users.rb
@@ -1,6 +1,6 @@
 class AddSystemUserFlagToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :system_user, :boolean, default: false, null: false
-    add_index :users, :system_user, where: "system_user = true", unique: true
+    add_index :users, :system_user, where: "system_user IS TRUE", unique: true
   end
 end
