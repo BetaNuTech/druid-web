c00a00ffc5d57927797b0cc46e5016d7225c80ed
Stu Carney <webstu@gmail.com>
2025-07-28 18:14:33 -0500

    Schema update for system_user index

--- db/schema.rb
+++ db/schema.rb
@@ -791,7 +791,6 @@ ActiveRecord::Schema.define(version: 2025_07_25_185348) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["system_user"], name: "index_users_on_system_user", unique: true, where: "(system_user = true)"
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
